4440ac0931542eefa889e4c10b9a3c782690e822
kleapaci <39514951+kleapaci@users.noreply.github.com>
2020-01-29 22:53:35 +0100

    change config.py

--- cert_verifier/config.py
+++ cert_verifier/config.py
@@ -1,2 +1,23 @@
+import os
+import configargparse
 
-infura_node = "https://ropsten.infura.io/v3/a70de76e3fd748cbb6dbb2ed49dda183"
+cwd = os.getcwd()
+CONFIG = None
+
+
+def add_arguments(p):
+    p.add('-c', '--my-config', required=False, env_var='CONFIG_FILE',
+          is_config_file=True, help='config file path')
+    p.add_argument('--infura_ropsten', help='infura ropsten', env_var='INFURA_ROPSTEN')
+    p.add_argument('--infura_mainnet', help='infura mainnet', env_var='INFURA_MAINNET')
+
+
+def get_config():
+    p = configargparse.getArgumentParser(default_config_files=[os.path.join(cwd, 'conf.ini')])
+    add_arguments(p)
+    parsed_config, _ = p.parse_known_args()
+
+    global CONFIG
+    CONFIG = parsed_config
+    return parsed_config
+
